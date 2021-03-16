f70dceb2e801aff410f18fa96140fa6f4c02b24f
Masahiro Hayashi <41661297+hayashimasa@users.noreply.github.com>
2021-03-16 20:28:23 +0100

    Incorporate Text_Encoder; make Aggregator adaptable to Encoder modules

--- NN.py
+++ NN.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from encoder import FC_Block
 
 class Aggregator(nn.Module):
     """Model for aggregating all input features
@@ -10,29 +11,30 @@ class Aggregator(nn.Module):
         img (nn.Module): image encoding model
     """
 
-    def __init__(self, cat, cont, img, h_dim=100):
-        self.name = 'Main Neural Network'
+    def __init__(self, cat=None, cont=None, img=None, text=None, h_dim=100):
+        self.name = 'Output Network'
         super(Aggregator, self).__init__()
         self.cat = cat
         self.cont = cont
         self.img = img
-        self.agg_dim = sum([model.out_dim for model in [cat, cont, img]])
+        self.text = text
+        names = ['cat', 'cont', 'img', 'text']
+        self.models = {
+            name: getattr(self, name) for name in names
+            if not getattr(self, name) is None
+        }
+        self.agg_dim = sum([model.out_dim for _, model in self.models.items()])
         self.h_dim = h_dim
-        self.fc = self.fc_block(self.agg_dim, self.h_dim)
+        self.fc = FC_Block(self.agg_dim, self.h_dim)
         self.out = nn.Linear(self.h_dim, 1)
 
     def forward(self, x):
         # x_img = torch.mean([self.img(x) for im in x['img']])
-        x_img = self.img(x['img'])
-        x = [self.cat(x['cat']), self.cont(x['cont']), x_img]
+        x = [model(x[name]) for name, model in self.models.items()]
+        #     self.cat(x['cat']), self.cont(x['cont']),
+        #     self.img(x['img']), self.text(x['text'])
+        # ]
         x = torch.cat(x, 1)
         x = self.fc(x)
         x = self.out(x)
         return x
-
-    def fc_block(self, in_dim, out_dim, p_drop=0.5):
-        fc = nn.Linear(in_dim, out_dim)
-        bn = nn.BatchNorm1d(out_dim)
-        relu = nn.ReLU(True)
-        dropout = nn.Dropout(p_drop)
-        return nn.Sequential(*[fc, bn, relu, dropout])
